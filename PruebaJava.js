d3d69b34b64e56beeafb0cf22bc7f3c5ab825d73
Aprendiz <Aprendiz@BOGDFPCGMFS4120.FormacionSena.RED>
2024-03-01 14:12:34 -0500

    modificacion

---
+++ PruebaJava.js
@@ -0,0 +1,13 @@
+//Entradas
+
+console.log("1. calcular el área de un triangulo: ");
+console.log("Ingrese la base: ");
+let base =10;
+console.log("Ingrese la altura");
+
+
+//proceso
+let resultado = (base * altura) / 2;
+
+//salida
+console.log(resultado);
