bc180175ccf34cad800be3ce8b0d19462ffacd5b
Aprendiz <Aprendiz@BOGDFPCGMFS4120.FormacionSena.RED>
2024-03-01 14:35:36 -0500

    modificaciones

---
+++ operaciones/ejercicio1.js
@@ -0,0 +1,14 @@
+//Entradas
+
+console.log("1. calcular el área de un triangulo: ");
+console.log("Ingrese la base: ");
+let base = 10;
+console.log("Ingrese la altura");
+let altura = 15;
+
+
+//proceso
+let resultado = (base * altura) / 2;
+
+//salida
+console.log(resultado);
